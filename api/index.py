f267276a505f0943b9b549fbd46a272af84172fb
Munya Mpofu <mpofumunya9@gmail.com>
2024-11-13 16:44:38 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print('testing......')
+    response = requests.get('https://api.my-ip.io/v2/ip.json')
+    print(response.text)
     return 'Hello, World!'
 
 @app.route('/about')
